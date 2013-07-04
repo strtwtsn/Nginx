1c9f9dcb8f855254b7407f8134371c67f5b32725
strtwtsn <stuart.watson@unep-wcmc.org>
2013-07-04 15:27:15 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p195.tar.gz
 tar -xvzf ruby-2.0.0-p195.tar.gz
 cd /usr/local/src/ruby-2.0.0-p195/
 ./configure
- make
+make
 sudo checkinstall --pkgname ruby_2.0 --pkgversion 1.0 --default 
+gem update --system 1.8.25
 /usr/local/bin/gem install bundler chef ruby-shadow --no-ri --no-rdoc
